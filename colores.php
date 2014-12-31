2a809055585df9bed1cf371d238fcd86d670d2b4
ismael_br7@hotmail.com <ismael_br7@hotmail.com>
2014-12-30 19:19:49 -0600

    Simple metodo load ajax
    Metodo load para cargar archivos del backend con ajax

---
+++ colores.php
@@ -0,0 +1,13 @@
+<?php
+	
+	if($_GET['color'] == 'verde'){
+		echo "el color verde es muy hermoso y fresco";
+	}
+
+	if($_GET['color'] == 'azul'){
+		echo "el color azul es muy frio";
+	}
+
+	if($_GET['color'] == 'rojo'){
+		echo "el color rojo es muy pasional";
+	}
